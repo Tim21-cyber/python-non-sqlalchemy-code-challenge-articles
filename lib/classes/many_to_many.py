934cbf97c639b72b56435de2e4d36dc935572b88
Timothy <timothy.ochieng1@student.moringaschool.com>
2025-08-21 00:12:58 +0300

    complete code challenge lab

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,38 +1,116 @@
 class Article:
     def __init__(self, author, magazine, title):
+        if not isinstance(author, Author):
+            raise Exception("author must be an Author instance")
+        if not isinstance(magazine, Magazine):
+            raise Exception("magazine must be a Magazine instance")
+        if not isinstance(title, str):
+            raise Exception("title must be a string")
+        if not (5 <= len(title) <= 50):
+            raise Exception("title must be between 5 and 50 characters")
+        
         self.author = author
         self.magazine = magazine
         self.title = title
+
+        author._articles.append(self)
+        magazine._articles.append(self)
+
+    @property
+    def title(self):
+        return self._title
         
 class Author:
     def __init__(self, name):
+        if not isinstance(name, str):
+            raise Exception("name must be a string")
+        if len(name.strip()) == 0:
+            raise Exception("name must be longer than 0 characters")
+        
         self.name = name
+        self._articles = []
+
+    @property
+    def name(self):
+        return self._name  # immutable
 
     def articles(self):
-        pass
+        return self._articles
 
     def magazines(self):
-        pass
+        return list({article.magazine for article in self._articles})
 
     def add_article(self, magazine, title):
-        pass
+        return Article(self, magazine, title)
 
     def topic_areas(self):
-        pass
+        if not self._articles:
+            return None
+        return list({article.magazine.category for article in self._articles})
 
 class Magazine:
+    _all = []
     def __init__(self, name, category):
+        if not isinstance(name, str):
+            raise Exception("name must be a string")
+        if not (2 <= len(name) <= 16):
+            raise Exception("name must be between 2 and 16 characters")
+        if not isinstance(category, str):
+            raise Exception("category must be a string")
+        if len(category.strip()) == 0:
+            raise Exception("category must be longer than 0 characters")
+        
         self.name = name
         self.category = category
+        self._articles = []
+
+        Magazine._all.append(self)
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise Exception("name must be a string")
+        if not (2 <= len(value) <= 16):
+            raise Exception("name must be between 2 and 16 characters")
+        self._name = value
+
+    @property
+    def category(self):
+        return self._category
+
+    @category.setter
+    def category(self, value):
+        if not isinstance(value, str):
+            raise Exception("category must be a string")
+        if len(value.strip()) == 0:
+            raise Exception("category must be longer than 0 characters")
+        self._category = value
 
     def articles(self):
-        pass
+        return self._articles
 
     def contributors(self):
-        pass
+        return list({article.author for article in self._articles})
 
     def article_titles(self):
-        pass
+        if not self._articles:
+            return None
+        return [article.title for article in self._articles]
 
     def contributing_authors(self):
-        pass
+        from collections import Counter
+        if not self._articles:
+            return None
+        author_counts = Counter(article.author for article in self._articles)
+        authors = [author for author, count in author_counts.items() if count > 2]
+        return authors if authors else None
+
+    @classmethod
+    def top_publisher(cls):
+        if not any(mag._articles for mag in cls._all):
+            return None
+        return max(cls._all, key=lambda mag: len(mag._articles))
